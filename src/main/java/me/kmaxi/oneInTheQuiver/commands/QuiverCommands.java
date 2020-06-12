7b251ccfde81fa5dc0cc690ca009619c6bb5d3c3
kmaxi <esst936@gmail.com>
2020-06-12 19:58:40 +0200

    update

--- src/main/java/me/kmaxi/oneInTheQuiver/commands/QuiverCommands.java
+++ src/main/java/me/kmaxi/oneInTheQuiver/commands/QuiverCommands.java
@@ -1,12 +1,17 @@
 package me.kmaxi.oneInTheQuiver.commands;
 
 import me.kmaxi.oneInTheQuiver.OneInTheQuiverMain;
+import me.kmaxi.oneInTheQuiver.gameHandler.PlayerManager;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.UUID;
 
 public class QuiverCommands implements CommandExecutor {
     private OneInTheQuiverMain plugin;
@@ -22,8 +27,30 @@ public class QuiverCommands implements CommandExecutor {
             return true;
         }
         Player player = (Player) sender;
+        if(!(player.hasPermission("quiver.commands"))){
+            player.sendMessage(ChatColor.RED + "Missing permissions!");
+            return true;
+        }
         Location loc = player.getLocation();
             if (args.length == 1) {
+                if (args[0].equalsIgnoreCase("points")){
+                    if (plugin.gameManager.allPlayerPoints.isEmpty()){
+                        sender.sendMessage(ChatColor.RED + "No one has any points right now!");
+                        return true;
+                    }
+                    int position = 1;
+                    ArrayList<Player> sortedPlayers = new ArrayList<>();
+                    for (UUID playerName: plugin.gameManager.allPlayerPoints.keySet()){
+                        Player p = Bukkit.getPlayer(playerName);
+                        sortedPlayers.add(p);
+                    }
+                    sortedPlayers.sort((playerA, playerB) -> plugin.gameManager.allPlayerPoints.get(playerA.getUniqueId()) - plugin.gameManager.allPlayerPoints.get(playerB.getUniqueId()));
+                    for(Player playerManager : sortedPlayers){
+                        sender.sendMessage(ChatColor.GRAY + String.valueOf(position) + ". " + playerManager.getName() + " (" + plugin.gameManager.allPlayerPoints.get(playerManager.getUniqueId()) + "p)");
+                        position++;
+                    }
+                    return true;
+            }
                 if (args[0].equalsIgnoreCase(plugin.commands.cmd2)){ //CMD2, which should set spawn location for time before game starts
                     plugin.getConfig().set("spawn", loc);
                     player.sendMessage(ChatColor.GREEN + "Spawn succesfully located!");
@@ -31,6 +58,7 @@ public class QuiverCommands implements CommandExecutor {
                     return true;
                 }
                 if (args[0].equalsIgnoreCase(plugin.commands.cmd3)){ //Command three which should start the game
+                    player.sendMessage(ChatColor.GREEN + "You have started the game");
                     plugin.gameManager.setUpGame();
                     return true;
                 }
